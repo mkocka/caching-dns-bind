666bcb4bd0fabe94c76834cb8871bc563219d061
Pavel Šimerda <pavlix@pavlix.net>
2017-01-02 15:14:25 +0100

    read name servers from /etc/resolv.conf

--- files/bind-run.sh
+++ files/bind-run.sh
@@ -1,5 +1,32 @@
-#!/bin/bash
+#!/usr/bin/python
 
-python /files/mako-render.py /files/named.conf.mako /config.yaml > /etc/named.conf
+from __future__ import print_function, unicode_literals
 
-/usr/sbin/named -u named -g
+import yaml
+import mako.template
+import subprocess
+
+# Read template
+with open("/files/named.conf.mako") as stream:
+    template = mako.template.Template(stream.read())
+
+# Read configuration file
+with open("/config.yaml") as stream:
+    data = yaml.load(stream)
+
+# Fallback to /etc/resolv.conf contents
+if not data.get("nameservers"):
+    if "nameservers" in data:
+	del data["nameservers"]
+    with open("/etc/resolv.conf") as stream:
+	for line in stream:
+	    line = line.strip()
+	    if line.startswith("nameserver "):
+		data.setdefault("nameservers", []).append(line.split()[1])
+
+# Write configuration file
+with open("/etc/named.conf", "w") as stream:
+    print(template.render(**data), file=stream)
+
+# Run the nameserver
+subprocess.check_call(["/usr/sbin/named", "-u", "named", "-g"])
